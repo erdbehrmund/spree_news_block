2f4b3b4cb58290c9273eab1dbce1a1a5b8b36348
Sergey Sytchewoj <erdbehrmund@gmail.com>
2013-01-09 15:35:27 +0200

    Added created_at to get_news helper

--- app/helpers/spree/news_item_helper.rb
+++ app/helpers/spree/news_item_helper.rb
@@ -1,5 +1,5 @@
 module Spree::NewsItemHelper
   def get_news
-    Spree::NewsItem.active.limit(Spree::NewsConfig[:news_per_block]).order("created_at desc").select("title, substr(body, 1, #{Spree::NewsConfig[:symbols_per_news]}) as body")
+    Spree::NewsItem.active.limit(Spree::NewsConfig[:news_per_block]).order("created_at desc").select("title, substr(body, 1, #{Spree::NewsConfig[:symbols_per_news]}) as body, created_at")
   end
 end
--- spree_news_block.gemspec
+++ spree_news_block.gemspec
@@ -2,7 +2,7 @@
 Gem::Specification.new do |s|
   s.platform    = Gem::Platform::RUBY
   s.name        = 'spree_news_block'
-  s.version     = '1.3.0.2'
+  s.version     = '1.3.0.3'
   s.summary     = 'News posting support for Spree applications'
   s.description = ''
   s.required_ruby_version = '>= 1.8.7'
